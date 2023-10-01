394b18538e4c9e6c6eb318ec398b9eaeaea71164
aoceguera3-LZCUZB <arocegue@ucsc.edu>
2023-10-01 16:43:29 -0700

    backend update

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import json
 #Hardcoded credentials for local use only
 conn = psycopg2.connect(host='localhost', database="nba_stats", user='postgres', password='postgres')
 
-columns = ['Id','Team', 'PTS', 'FG', 'FGA', '2PA', '3P', '3PA', 'eFG%',
+columns = ['Id','Team', 'PTS', 'FG', 'FGA', '2PA', '3P', '3PA', 'eFG%', 'Home/Away',
        'ORtg', 'FTr', '3PAr', 'TS%', 'FT/FGA', 'ORtg_opponent', 'FTr_opponent',
        '3PAr_opponent', 'TS%_opponent', 'eFG%_opponent', 'FT/FGA_opponent']
 #################################################
@@ -27,11 +27,11 @@ nn_model = load_model("NBA_Model_V2.h5")
 #################################################
 # Flask Routes
 #################################################
-def getTeam(team: str):
+def getTeam(team1: str, team2: str):
     cur = conn.cursor()
 
     #Using Limit to test query without bloating console
-    cur.execute('SELECT * FROM nba WHERE nba.Team = %s', (team, ))
+    cur.execute('SELECT * FROM nba WHERE nba.Team = %s OR nba.Team = %s', (team1, team2, ))
     team_data = cur.fetchall()
     cur.close()
     return team_data
@@ -39,13 +39,13 @@ def getTeam(team: str):
 def changeTypes(df):
     return df.astype({
         'Team': "string",
+        'Home/Away': "string"
     })
 
-def transformAndPredict(df, i, k):
+def transformAndPredict(df):
     y = df["PTS"]
     X = df.drop(["Id", "Team", "PTS"], axis=1)
-    X["Home/Away_Away"] = k
-    X["Home/Away_Home"] = i
+    X = pd.get_dummies(X, columns=["Home/Away"])
     print(X.head())
     # Create a StandardScaler instances
     scaler = StandardScaler()
@@ -59,8 +59,8 @@ def transformAndPredict(df, i, k):
 
 
     prediction = nn_model.predict(X_train_scaled)
-
-    return int(round(prediction[0][0]))
+    flat_list = [round(item) for sublist in prediction for item in sublist]
+    return flat_list
 
 
 
@@ -78,32 +78,33 @@ def data(team1 = None, team2 = None):
         return render_template_string('Team Not Found {{ errorCode }}', errorCode='404'), 404
     
     #Get Team Data from database
-    team1_data = getTeam(team1)
-    team2_data = getTeam(team2)
-    
-    if team1_data == [] or team2_data == []:
+    teams_data = getTeam(team1, team2)
+
+    if teams_data == []:
         return render_template_string('Team Not Found {{ errorCode }}', errorCode='404'), 404
 
     #Transform to DF
-    team1_data_df = pd.DataFrame(team1_data, columns=columns)
-    team2_data_df = pd.DataFrame(team2_data, columns=columns)
+    teams_data_df = pd.DataFrame(teams_data, columns=columns)
+
     #Change from Object to String
-    team1_data_df = changeTypes(team1_data_df)
-    team2_data_df = changeTypes(team2_data_df)
-    print(team1_data_df)
-    print(team2_data_df)
-    #Hardcoded home vs away
-    i = 1
-    k = 0
-    
-    team1_score = transformAndPredict(team1_data_df, i, k)
-    i-=1
-    k+=1
-    team2_score = transformAndPredict(team2_data_df, i, k)
+    teams_data_df = changeTypes(teams_data_df)
+    print(teams_data_df)
+    #Set Home/Away
+    teams_data_df.loc[teams_data_df["Team"] == team1, 'Home/Away'] = 'Home'
+    teams_data_df.loc[teams_data_df["Team"] == team2, 'Home/Away'] = 'Away'
+
+    #Check to see if Home team is first in the row, if not swap order
+    #This helps keep track of respective prediction
+    b, c = teams_data_df.iloc[0], teams_data_df.iloc[1]
+    if b["Home/Away"] != "Home":
+        temp = teams_data_df.iloc[0].copy()
+        teams_data_df.iloc[0] = c
+        teams_data_df.iloc[1] = temp
+    team_scores = transformAndPredict(teams_data_df)
     # conn.close()
     return json.loads(json.dumps({
-        "team_1": team1_score,
-        "team_2": team2_score
+        "team_1": team_scores[0],
+        "team_2": team_scores[1]
     }))
 
 if __name__ == '__main__':
